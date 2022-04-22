0e17755d96b82bc2f2b80641571544a6bfda0d03
daniraVazquezC <danira.vazquez2811@gmail.com>
2022-04-21 20:03:59 -0500

    add methods to links_controller

---
+++ app/controllers/api/v1/links_controller.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class LinksController < ApplicationController
+      before_action :set_link, only: %i[update destroy]
+      before_action :authenticate_user!
+
+      # GET /api/v1/links
+      def index
+        render json: current_user.links
+      end
+
+      # POST /api/v1/links
+      def create
+        link = Link.new(url: params[:url], user: current_user)
+        if link.save
+          render json: link, status: :created
+        else
+          render json: link.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /api/v1/links/:id
+      def update
+        if @link.update(url: params[:url])
+          render json: @link, status: :ok
+        else
+          render json: @link.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /api/v1/links/:id
+      def destroy
+        @link.destroy
+      end
+
+      private
+
+      def set_link
+        @link = Link.find_by(short_link_code: params[:id])
+        raise ActiveRecord::RecordNotFound if @link.blank?
+      end
+    end
+  end
+end
